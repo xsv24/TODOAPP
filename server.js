dc55d411ff80051141f60c7cd2bc0a7710ac094a
Thomas Pearson <thomas.pearson@selectedinterventions.com>
2020-03-26 01:14:44 +0000

    fix port

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', function(req, res) {
+ 
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const port = process.env.PORT || 8080;
-app.listen(8080);
+app.listen(port);
 
 console.log('serving on port:', port);
